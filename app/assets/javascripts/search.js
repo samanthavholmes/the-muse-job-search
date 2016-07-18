810e6b89467090dcb444ce69f63799032f8a0faa
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-18 19:13:37 -0400

    start basic design

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $("#company-search-form").autocomplete({
+      source: availableTags
+    });
+  });
+  $("#company-search-form").on("click", "input", function(e){
+    console.log("hi");
+  })
+});
