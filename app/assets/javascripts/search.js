2d1bd2249975f83474d14956c4b355d361aab7ed
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-18 22:40:53 -0400

    add jquery to search param tabs

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,31 +1,11 @@
 $(document).ready(function(){
-  $(function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $("#company-search-form").on("click", function(e){
-      $('#company-search-form').autocomplete({source: availableTags});
-    });
+  $('.active').hide();
+  $('.search-option').on("click", function(e){
+    e.preventDefault();
+    $('.active').hide();
+    $('.active').removeClass('active');
+    var id = "#" + this.id + "-list"
+    $(id).show();
+    $(id).addClass("active");
   })
 });
