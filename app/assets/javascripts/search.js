67e2021c323fa54b4609b0129532e91850b21853
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-19 19:59:35 -0400

    Successfully make API call with multiple parameters, add helper methods for the views

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -18,16 +18,13 @@ $(document).ready(function(){
       return this.name;
     });
     var locations = $('#location-search-form').tokenfield('getTokensList', ';').split(';');
-    // debugger;
-    // var search_queries = {companies: companies, levels: levels, categories: categories, locations: locations}
-    var search_queries = {companies: $.makeArray(companies), levels: $.makeArray(levels), categories: $.makeArray(categories), locations: locations}
+    var search_queries = {companies: companies, levels: $.makeArray(levels), categories: $.makeArray(categories), locations: locations}
     $.ajax({
       url: '/search',
       method: 'POST',
-      dataType: 'json',
       data: search_queries
     }).done(function(response){
-      debugger;
+      $('body').replaceWith(response)
     })
  })
   })
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,47 @@
 module SearchHelper
+
+  def company_name(job_listing)
+    job_listing["company"]["name"]
+  end
+
+  def levels(job_listing)
+    levels = []
+    job_listing["levels"].each do |level|
+      levels << level["name"]
+    end
+    return levels
+  end
+
+  def job_title(job_listing)
+    job_listing["name"]
+  end
+
+  def job_description(job_listing)
+    render html: job_listing["contents"].html_safe
+  end
+
+  def categories(job_listing)
+    categories = []
+    job_listing["categories"].each do |category|
+      categories << category["name"]
+    end
+    return categories
+  end
+
+  def locations(job_listing)
+    locations = []
+    job_listing["locations"].each do |location|
+      locations << location["name"]
+    end
+    return locations
+  end
+
+  def url_for_full_listing(job_listing)
+    job_listing["refs"]["landing_page"]
+  end
+
+  def listing_date(job_listing)
+    job_listing["publication_date"]
+  end
+
 end
