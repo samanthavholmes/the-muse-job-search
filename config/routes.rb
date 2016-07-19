2bfb1d12aceb0ba01add3bccf072732140f13b71
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-18 23:52:43 -0400

    format autocomplete fields and add show action to search controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'search#index'
 
+  post "/search" => 'search#show'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
