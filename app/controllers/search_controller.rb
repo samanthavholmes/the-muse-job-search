a7b25606a0ebe3c70d64fa6b5c0cdb85192989ec
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-19 20:22:46 -0400

    add sessions to allow for next api calls

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,16 @@ class SearchController < ApplicationController
   include SearchSupporter
 
   def index
+    session.clear
+    session[:page] = 0
   end
 
   def show
     results = get_api_results(get_url(params))
+    session[:page] += 1
     if request.xhr?
+      render :show, layout: false, locals: {results: results}
+    else
       render :show, layout: false, locals: {results: results}
     end
   end
