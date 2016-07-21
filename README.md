de1e811c42cf551d892f133e06951de6b222e545
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-21 14:48:00 -0400

    start testing controllers/concers, finish testing for helpers

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# the-muse-job-search
-Job search engine using The Muse's API
+== README
+
+The Muse Job Search
+==========
+
+A web application that allows a user to search for job listings using.
+
+Technologies
+------------
+
+- Ruby on Rails backend
+- jQuery/AJAX to enhance user experience
+- HTML5/CSS3
+  - Bootstrap framework used to enhance design
+- RSpec testing
+- SQLite3 database
+
+Steps
+-----
+
+- Download SQL file
+- Create SQLite3 database from SQL file
+- Create new rails app
+- Open database when user hits search
+- Start a queue that adds nodes based off direct connections
+  - Uses a breadth-first search method
+  - First-in, first-out
+- Stops adding to the queue as soon as the target is found in it
+- Finds the path of names to return to user
+
+
+Process
+-------
+
+I initially thought a graph would be the best way to solve this problem. I created a graph class and a method "populate_graph", which created an adjacency list of every item linked to an array of their direct children connections. The issue I ran into was how much time it took to build this. It took ~8 minutes to build the graph of the entire database. I then thought about making an adjacency matrix but a matrix of ~5,700,000 x 5,700,000 took even longer. I also decided to create another application to build the graph and then push it into a CSV file that I could use in this application to search through, which cut the time in half. I tried using that graph to find the children connections, but it was no faster than making the sql queries. (I kept this code commented out in the graph.rb file under initalizers and included the create_csv app in the zip file). That is when I created my own solution using a breadth-first search queue.
+
+I was deciding between using a breadth-first search or a depth-first search because I wasn't sure if recursion or iteration would be the best way to go. I decided with breadth-first because since there are over 100 million connections, getting to the last children of each id before finding the target would take an exponential amount of time.
+
+My solution works quickly for some queries, but others take a lot of time. Some working examples are:
+  - Michael Jackson to Hotel
+  - The Beatles to Elvis
+  - ! to 1979
+
+I also researched about the graph database neo4j, which seems like it could be a viable solution to seraching through a big database, such as this one.
---
+++ spec/helpers/search_helper_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe "SearchHelper" do
+  include SearchHelper
+
+  let!(:job_listing) {{"locations"=>[{"name"=>"New York City Metro Area"}],
+ "company"=>{"name"=>"Fueled", "id"=>162, "short_name"=>"fueled"},
+ "short_name"=>"operations-intern-4677b6",
+ "type"=>"external",
+ "refs"=>{"landing_page"=>"https://www.themuse.com/jobs/fueled/operations-intern-4677b6"},
+ "id"=>28540,
+ "publication_date"=>"2015-11-17T22:07:10.501307Z",
+ "categories"=>[{"name"=>"HR & Recruiting"}, {"name"=>"Operations"}],
+ "name"=>"Operations Intern",
+ "model_type"=>"jobs",
+ "contents"=>
+  "<p><strong>WHAT WE’RE UP TO</strong></p>\n<p>We're leading the charge in a world of apps that do stuff, and do it with style. It's no longer sufficient for brands to just throw out apps that 'delight' without much substance. We work with our clients – from small startups to big brands – to figure out how to achieve their marketing and business goals through smart, high-functioning apps and websites that genuinely engage their users and keep them coming back for more. We also run one of the top coworking spaces in Manhattan, located in the heart of Soho and featured in The New York Times. The Fueled Collective is home to over 30 startups, and boasts a well-stocked snack wall, a ping pong table, and incredible views of the Manhattan skyline.</p>\n<p>Here's a cool video showing what its like!</p>\n<p><strong>WHERE YOU FIT IN</strong> </p>\n<p>The Operations internship at Fueled is a 3-month long training program for college students interested in Human Resources and Recruiting. As an Operations intern, you will work closely with the Operations Manager and Talent Manager, and will gain a robust understanding of the activities involved in running day-to-day operations of a tech startup. You will gain valuable exposure to and experience in areas that include, but are not limited to, Human Resources, Recruiting, Orientation &amp; Training, Resourcing, Finance, and Process Documentation. Throughout the internship, you will be actively involved in the daily function of the company, and will build fundamental professional skills such as effective time management and organizational habits.</p>\n<p><strong>ROLES AND RESPONSIBILITIES FOR THE POSITION INCLUDE:</strong></p>\n<ul>\n<li>Helping to manage the pipeline for all of Fueled’s NYC-based technical/operational roles.</li>\n<li>Communicating with applicants through email, phone, ATS, and LinkedIn.</li>\n<li>Drafting, editing, and posting job requisitions in an on-brand voice.</li>\n<li>Supporting the Operations Manager with administrative tasks and special projects.</li>\n</ul>\n<p><strong>YOU WILL LEARN HOW TO:</strong></p>\n<ul>\n<li>Source candidates for tech industry roles using cutting-edge sourcing methods.</li>\n<li>Utilize third-party HR/recruiting platforms.</li>\n<li>Screen candidates.</li>\n<li>Perform HR functions including onboarding, payroll, resource management, and offboarding.</li>\n</ul>\n<p>The ideal candidate will be a quick learner and a hard worker with a positive attitude. An eagerness to learn and excitement for the tech industry is far more important than previous experience.</p>\n<p>Candidates must be able to receive college credit for this internship. </p>",
+ "levels"=>[{"name"=>"Internship", "short_name"=>"internship"}],
+ "tags"=>[]}
+}
+
+  describe "company_name" do
+    it "returns the company name from the api results" do
+      expect(company_name(job_listing)).to eq("Fueled")
+    end
+  end
+
+  describe "levels" do
+    it "returns an array of the job levels from the api results" do
+      expect(levels(job_listing)).to eq ["Internship"]
+    end
+  end
+
+  describe "job_title" do
+    it "returns the job title from the api results" do
+      expect(job_title(job_listing)).to eq "Operations Intern"
+    end
+  end
+
+  describe "job_description" do
+    it "renders the html results of the job description from the api results" do
+      expect(job_description(job_listing)).to eq "<p><strong>WHAT WE’RE UP TO</strong></p>\n<p>We're leading the charge in a world of apps that do stuff, and do it with style. It's no longer sufficient for brands to just throw out apps that 'delight' without much substance. We work with our clients – from small startups to big brands – to figure out how to achieve their marketing and business goals through smart, high-functioning apps and websites that genuinely engage their users and keep them coming back for more. We also run one of the top coworking spaces in Manhattan, located in the heart of Soho and featured in The New York Times. The Fueled Collective is home to over 30 startups, and boasts a well-stocked snack wall, a ping pong table, and incredible views of the Manhattan skyline.</p>\n<p>Here's a cool video showing what its like!</p>\n<p><strong>WHERE YOU FIT IN</strong> </p>\n<p>The Operations internship at Fueled is a 3-month long training program for college students interested in Human Resources and Recruiting. As an Operations intern, you will work closely with the Operations Manager and Talent Manager, and will gain a robust understanding of the activities involved in running day-to-day operations of a tech startup. You will gain valuable exposure to and experience in areas that include, but are not limited to, Human Resources, Recruiting, Orientation &amp; Training, Resourcing, Finance, and Process Documentation. Throughout the internship, you will be actively involved in the daily function of the company, and will build fundamental professional skills such as effective time management and organizational habits.</p>\n<p><strong>ROLES AND RESPONSIBILITIES FOR THE POSITION INCLUDE:</strong></p>\n<ul>\n<li>Helping to manage the pipeline for all of Fueled’s NYC-based technical/operational roles.</li>\n<li>Communicating with applicants through email, phone, ATS, and LinkedIn.</li>\n<li>Drafting, editing, and posting job requisitions in an on-brand voice.</li>\n<li>Supporting the Operations Manager with administrative tasks and special projects.</li>\n</ul>\n<p><strong>YOU WILL LEARN HOW TO:</strong></p>\n<ul>\n<li>Source candidates for tech industry roles using cutting-edge sourcing methods.</li>\n<li>Utilize third-party HR/recruiting platforms.</li>\n<li>Screen candidates.</li>\n<li>Perform HR functions including onboarding, payroll, resource management, and offboarding.</li>\n</ul>\n<p>The ideal candidate will be a quick learner and a hard worker with a positive attitude. An eagerness to learn and excitement for the tech industry is far more important than previous experience.</p>\n<p>Candidates must be able to receive college credit for this internship. </p>"
+    end
+  end
+
+  describe "locations" do
+    it "returns an array of the job locations from the api results" do
+      expect(locations(job_listing)).to eq ["New York City Metro Area"]
+    end
+  end
+
+
+  describe "url_for_full_listing" do
+    it "returns the url for the job listing on The Muse" do
+      expect(url_for_full_listing(job_listing)).to eq "https://www.themuse.com/jobs/fueled/operations-intern-4677b6"
+    end
+  end
+
+  describe "listing_date" do
+    it "returns the listing date from the api results" do
+      expect(listing_date(job_listing)).to eq "2015-11-17T22:07:10.501307Z"
+    end
+  end
+end
