e4d79e9e4c317a4144ae2799acb5a42ee95358b4
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-21 13:31:06 -0400

    add design for search result page, make all job listings even height

--- app/controllers/concerns/search_supporter.rb
+++ app/controllers/concerns/search_supporter.rb
@@ -2,30 +2,38 @@ require 'open-uri'
 
 module SearchSupporter
 
+  def reset_session
+    session[:page] = 0
+    session[:query] = ""
+  end
+
   def get_url(params)
     session[:url] = "https://api-v2.themuse.com/jobs?page=" + "#{session[:page]}"
-    if !session[:query]
-      if params[:companies] != [""]
+    if session[:query] == ""
+      if params[:companies] && params[:companies] != [""]
         companies = params[:companies].map! {|c| c.gsub(" ", "+")}.join("&company=")
         company_query = "&company=" + companies
         session[:url] += company_query
+        session[:query] += company_query
       end
       if params[:levels]
         levels = params[:levels].map! {|c| c.gsub(" ", "+")}.join("&level=")
         level_query = "&level=" + levels
         session[:url] += level_query
+        session[:query] += level_query
       end
       if params[:categories]
         categories = params[:categories].map! {|c| c.gsub(" ", "+").gsub("&","%26")}.join("&category=")
         category_query = "&category=" + categories
         session[:url] += category_query
+        session[:query] += category_query
       end
-      if params[:locations] != [""]
+      if params[:locations] && params[:locations] != [""]
         locations = params[:locations].map! {|c| c.gsub(" ", "+").gsub(",","%2C")}.join("&location=")
         location_query = "&location=" + locations
         session[:url] += location_query
+        session[:query] += location_query
       end
-        session[:query] = true
     end
     return session[:url]
   end
